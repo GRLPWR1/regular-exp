413f3496a9e2575fc6978175e3daa5e9813459d0
GRLPWR1 <110125337+GRLPWR1@users.noreply.github.com>
2023-03-29 06:55:36 +0300

    grabAllSNILS passed the test

--- snils.js
+++ snils.js
@@ -4,24 +4,26 @@
 
 // Определи, есть ли во входной строке номер СНИЛС.
 function hasSNILS(string) {
-    let regexp = /(\d{3}-){2}\d{3}\s\d{2}/g;
+    let regexp = /(\d{3}-){2}\d{3}\s\d{2}\b/g;
     return regexp.test(string);
 };
 
-console.log(hasSNILS('234-600-142 22'))
+console.log(hasSNILS('234-600-142 22'));
 
 // Найди и верни номер СНИЛС из строки.
 function grabSNILS(string) {
-    let regexp = /(\d{3}-){2}\d{3}\s\d{2}/;
+    let regexp = /(\d{3}-){2}\d{3}\s\d{2}\b/;
     let result = string.match(regexp);
     return result[0];
 }
-console.log(grabSNILS("The number is 234-600-142 22"))
+console.log(grabSNILS("The number is 234-600-142 22"));
 
 // Найди и верни все номера СНИЛС, что есть в строке.
 function grabAllSNILS(string) {
-
+    let regexp = /(\d{3}-){2}\d{3}\s\d{2}\b/g;
+    return string.match(regexp);
 }
+console.log(grabAllSNILS("The numbers are 350-802-07 94, 234-600-142 w2, and 013-605-876 944"));
 
 //console.log(grabAllSNILS("The numbers are 350-802-074 94, 234-600-142 22, and 013-605-876 94"))
 
